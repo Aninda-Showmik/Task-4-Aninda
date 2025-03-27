fb161239bf59ebbaf4549e09257b755d09605be5
Aninda Das <139395892+Aninda-Showmik@users.noreply.github.com>
2025-03-27 21:29:14 +0600

    Update db.js

--- db.js
+++ db.js
@@ -4,9 +4,11 @@ const db = mysql.createConnection({
     host: 'fdb1029.awardspace.net',  // Database host (provided by AwardSpace)
     user: '4610662_userdetails',      // Database username (provided by AwardSpace)
     password: '182001612@Ani',        // Updated password
-    database: '4610662_userdetails'   // Database name (created on AwardSpace)
-    port: 3306
+    database: '4610662_userdetails',  // Database name (created on AwardSpace)
+    port: 3306,                       // MySQL default port
+    connectTimeout: 10000,            // Timeout for connection attempts (10 seconds)
 });
+
 // Connect to MySQL
 db.connect((err) => {
     if (err) {
